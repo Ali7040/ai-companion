20351f54759d703debbd697f41b8f4fa8f77750d
Ali Haider <ha4219466@gmail.com>
2023-09-13 12:56:21 +0500

    update file

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,12 +1,15 @@
+import { Categories } from "@/components/Categories";
 import { SearchInput } from "@/components/Search-input";
+import prismadb from "@/lib/prismadb";
 import { UserButton } from "@clerk/nextjs";
 
-const rootPage = () => {
+const RootPage = async () => {
+    const categories = await prismadb.category.findMany()
     return ( 
         <div className="h-full p-4 space-y-2">
             <SearchInput />
+            <Categories data={categories} />
         </div>
      );
 }
- 
-export default rootPage;
+export default RootPage;
